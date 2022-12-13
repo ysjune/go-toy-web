aef484e00d023a7f870669403a4862a90670e9aa
ysjune <ysjune@scglab.com>
2022-12-13 16:18:13 +0900

    1. web handler

--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Foo!")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello World")
 	})
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello Bar!")
+	})
+
+	http.Handle("/foo", &fooHandler{})
 
 	http.ListenAndServe(":3000", nil)
 }
